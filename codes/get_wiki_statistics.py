17f737b89fa6822c3a768ea166224251df46c8e9
1MmM1 <lin-family@live.com>
2021-03-17 10:54:36 -0700

    Remove comments and make command line argument required

--- codes/get_wiki_statistics.py
+++ codes/get_wiki_statistics.py
@@ -8,7 +8,7 @@ from collections import Counter
 LOG_INTERVAL = 1000000
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--wikifile", help="File path for wikipedia passages file", default="dpr_data_dir/data/wikipedia_split/psgs_w100.tsv", type=str)
+parser.add_argument("--wikifile", help="File path for wikipedia passages file", type=str, required=True)
 args = parser.parse_args()
 data_path = args.wikifile
 
@@ -16,28 +16,26 @@ print("Processing", data_path, "...")
 
 data = []
 
-line_index = 0
+log_index = 0
 if data_path[-3:] == ".gz":
    with gzip.open(data_path, "rb") as f:
       _ = f.readline()
       for line in f:
          data.append(line.decode().strip().split("\t")[2])
-         if line_index % LOG_INTERVAL == 0:
-            print("Currently at line", line_index)
-         line_index += 1
+         if log_index % LOG_INTERVAL == 0:
+            print("Currently at line", log_index)
+         log_index += 1
 else:
    with open(data_path, "r") as f:
       _ = f.readline()
       for line in f:
          data.append(line.strip().split("\t")[2])
-         if line_index % LOG_INTERVAL == 0:
-            print("Currently at line", line_index)
-         line_index += 1
+         if log_index % LOG_INTERVAL == 0:
+            print("Currently at line", log_index)
+         log_index += 1
 print("Finished processing file.")
 
 data = np.asarray(data, dtype=str)
-# title_counts = Counter(data[:,2])
 print("\nDataset statistics:")
 print(data.shape[0], "passages")
-# print(len(title_counts), "unique titles")
 print(len(set(data)), "unique titles")
